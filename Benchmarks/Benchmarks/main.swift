193dc5cc4d3f88b0621260527b4fd3ee9a72773c
Karol Wieczorek <karol.wieczorek@outlook.com>
2017-08-18 08:47:28 +0200

    Added empty Benchmarks project

---
+++ Benchmarks/Benchmarks/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Benchmarks
+//
+//  Created by Karol Wieczorek on 18.08.2017.
+//  Copyright © 2017 Karol Wieczorek. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
