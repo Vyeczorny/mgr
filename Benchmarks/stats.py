ceb0c159fec7b871eaf790ea3aa5ede76f87dc7a
Karol Wieczorek <karol.wieczorek@outlook.com>
2019-03-03 18:18:37 +0100

    Added summary table

--- Benchmarks/stats.py
+++ Benchmarks/stats.py
@@ -4,23 +4,37 @@ if len(sys.argv) < 2:
     print 'Needs argument'
     exit(1)
 
+if len(sys.argv) >= 3:
+    swiftIndex = int(sys.argv[2])
+else:
+    swiftIndex = 1
+
+if len(sys.argv) >= 4:
+    objCIndex = int(sys.argv[3])
+else:
+    objCIndex = 2
+
 fileName = sys.argv[1]
 diffs = map(
-    lambda line: (line[1] / line[2], line[2] / line[1]),
+    lambda line: (line[swiftIndex] / line[objCIndex ]),
     map(
         lambda line: map(float, line.split('\t')),
         open(fileName, "r").read().split('\n')
     )
 )
 
-swiftDiffs = map(lambda tuple: tuple[0], diffs)
-objCDiffs = map(lambda tuple: tuple[1], diffs)
+# swiftDiffs = map(lambda tuple: tuple[0], diffs)
+# objCDiffs = map(lambda tuple: tuple[1], diffs)
 
-avg = [
-    sum(swiftDiffs) / len(swiftDiffs),
-    sum(objCDiffs) / len(objCDiffs)
-]
+# avg = [
+    # sum(swiftDiffs) / len(swiftDiffs),
+    # sum(objCDiffs) / len(objCDiffs)
+# ]
 
-print diffs
-print 'Swift: ', "{:.2f}".format(avg[0] * 100), '%'
-print 'Obj-C: ', "{:.2f}".format(avg[1] * 100), '%'
+avg = sum(diffs) / len(diffs)
+result = avg * 100 - 100
+
+print [i * 100 for i in diffs]
+# print 'Swift: ', "{:.2f}".format(avg[0] * 100), '%'
+# print 'Obj-C: ', "{:.2f}".format(avg[1] * 100), '%'
+print 'Swift to Obj-C: ', "{:.2f}".format(result), '%'
